d95338bde33ce6c4e7210a899baf8a2f6348b169
Sytsykhovskyi Roman <roman@recrd.com>
2025-12-18 11:25:39 +0200

    init

---
+++ internal/candidates/candidates.go
@@ -0,0 +1,71 @@
+package candidates
+
+import (
+	"errors"
+	"fmt"
+	"net/mail"
+	"time"
+)
+
+var ErrNoResultsFound error = errors.New("no results found")
+
+type Candidate struct {
+	ID        string
+	FirstName string
+	LastName  string
+	Email     mail.Address
+	CreatedAt time.Time
+}
+
+type Manager struct {
+	candidates []Candidate
+}
+
+func NewManager() *Manager {
+	return &Manager{}
+}
+
+func (m *Manager) AddCandidate(firstName string, lastName string, email string) error {
+	if firstName == "" {
+		return fmt.Errorf("invalid first name: %q", firstName)
+	}
+
+	if lastName == "" {
+		return fmt.Errorf("invalid last name: %q", lastName)
+	}
+
+	existingCandidate, err := m.GetCandidateByName(firstName, lastName)
+	if err != nil && !errors.Is(err, ErrNoResultsFound) {
+		return fmt.Errorf("error checking if candidate is already present: %v", err)
+	}
+
+	if existingCandidate != nil {
+		return errors.New("candidate with this name already exists")
+	}
+
+	parsedAddress, err := mail.ParseAddress(email)
+	if err != nil {
+		return fmt.Errorf("invalid email: %s", email)
+	}
+
+	newCandidate := Candidate{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     *parsedAddress,
+		CreatedAt: time.Now(),
+	}
+
+	m.candidates = append(m.candidates, newCandidate)
+
+	return nil
+}
+
+func (m *Manager) GetCandidateByName(first string, last string) (*Candidate, error) {
+	for i, candidate := range m.candidates {
+		if candidate.FirstName == first && candidate.LastName == last {
+			result := m.candidates[i]
+			return &result, nil
+		}
+	}
+	return nil, ErrNoResultsFound
+}
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"test/internal/candidates"
+)
+
+type CandidateData struct {
+  FirstName string
+  LastName string
+  Email string
+}
+
+type Server struct {
+  candidatesManager *candidates.Manager
+}
+
+func main() {
+  candidatesManager := candidates.NewManager()
+
+   s := Server{
+    candidatesManager: candidatesManager,
+  }
+
+   mux := http.NewServeMux()
+
+  mux.HandleFunc("POST /candidates", s.addCandidate)
+  mux.HandleFunc("POST /get-candidates", s.getCandidate)
+
+
+  log.Fatal( http.ListenAndServe(":8080", mux))
+}
+
+func (s *Server) addCandidate(w http.ResponseWriter, r *http.Request) {
+	if !validateContentType(w, r) {
+		return
+	}
+
+  // limit to 1MB
+	requestBody := http.MaxBytesReader(w, r.Body, 1048576)
+
+	decoder := json.NewDecoder(requestBody)
+	decoder.DisallowUnknownFields()
+
+  var candidateData CandidateData
+
+  err := decoder.Decode(&candidateData)
+
+  if err != nil {
+		slog.Error("error decoding addCandidate request body", "err", err)
+		http.Error(w, "bad request body", http.StatusBadRequest)
+		return
+	}
+
+  err = s.candidatesManager.AddCandidate(candidateData.FirstName, candidateData.LastName, candidateData.Email)
+
+  if err != nil {
+		http.Error(w, fmt.Sprintf("error adding candidate: %v\n", err), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s *Server) getCandidate(w http.ResponseWriter, r *http.Request) {
+	if!validateContentType(w, r) {
+		return
+	}
+
+	// limit to 1MB
+	requestBody := http.MaxBytesReader(w, r.Body, 1048576)
+
+	decoder := json.NewDecoder(requestBody)
+	decoder.DisallowUnknownFields()
+
+	var candidateData CandidateData
+
+	err := decoder.Decode(&candidateData)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error decoding request body: %v\n", err), http.StatusBadRequest)
+		return
+	}
+
+	candidate, err := s.candidatesManager.GetCandidateByName(candidateData.FirstName, candidateData.LastName)
+	if err != nil {
+		if errors.Is(err, candidates.ErrNoResultsFound) {
+			http.Error(w, "no candidate found", http.StatusNotFound)
+		} else {
+			slog.Error("error retrieving candidate", "err", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	converted := convertCandidateToCandidateData(candidate)
+
+	marshalled, err := json.Marshal(converted)
+	if err != nil {
+		slog.Error("error marshalling getCandidate response", "err", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(marshalled)
+	if err != nil {
+		// headers are set by write call, best we can do is log an error
+		slog.Error("error writing getCandidate response body", "err", err)
+	}
+}
+
+func convertCandidateToCandidateData(u *candidates.Candidate) *CandidateData {
+	converted := CandidateData{
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Email:     u.Email.Address,
+	}
+
+	return &converted
+}
+
+func validateContentType(w http.ResponseWriter, r *http.Request) bool {
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		http.Error(w, fmt.Sprintf("unsupported Content-Type header %q", contentType), http.StatusUnsupportedMediaType)
+		return false
+  }
+  return true
+}
